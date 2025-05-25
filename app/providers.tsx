c46db0c5d23ccafcbafbb23d359252c195e9f284
Vivienne Kay <viviennekay@Viviennes-MacBook-Air.local>
2025-05-25 10:19:45 -0400

    fix: remove broken next-themes type import

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,28 @@
-"use client"
+'use client'
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import * as React from 'react'
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  return <NextThemesProvider>{children}</NextThemesProvider>
+interface ThemeProviderProps {
+  children: React.ReactNode
+  attribute?: string
+  defaultTheme?: string
+  enableSystem?: boolean
+}
+
+export function ThemeProvider({
+  children,
+  attribute = 'class',
+  defaultTheme = 'system',
+  enableSystem = true,
+}: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute={attribute}
+      defaultTheme={defaultTheme}
+      enableSystem={enableSystem}
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
